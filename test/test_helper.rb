9422c16b0ddf7e3eefdc1b17a07b59188a378ea3
Abbas Ali <absali6693@gmail.com>
2015-07-15 19:10:03 +0530

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,15 +5,14 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical
-  # order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-   def is_logged_in?
+  # Returns true if a test user is logged in.
+  def is_logged_in?
     !session[:user_id].nil?
   end
 
+  # Logs in a test user.
   def log_in_as(user, options = {})
     password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
