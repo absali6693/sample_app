770fda44a25e116e44d941c665ac14b0b852efcb
Abbas Ali <absali6693@gmail.com>
2015-07-18 00:15:47 +0530

    Add password resets & email configuration

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
+require "smtp_tls"
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+    ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => "587",
+    :user_name      => "absali6693@gmail.com",
+    :password       => "Abbas@6693",
+    :authentication => "plain"
+  }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,10 +62,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'absali6693.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
     :port           => "587",
@@ -75,7 +75,6 @@ Rails.application.configure do
     :authentication => "plain",
     :enable_starttls_auto => true
   }
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
