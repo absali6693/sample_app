ea220dac0233b060f4e5a968fd7870e2e7c083d9
Abbas Ali <absali6693@gmail.com>
2015-07-21 19:41:17 +0530

    correction

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.default_url_options = { :host => 'absali6693.herokuapp.com'}
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
     :port           => "587",
