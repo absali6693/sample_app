95cf4019ac630b0f24ea6c50d2da4a370313d584
Abbas Ali <absali6693@gmail.com>
2015-07-21 19:31:04 +0530

    correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
+  host = 'absali6693.herokuapp.com'
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.gmail.com",
