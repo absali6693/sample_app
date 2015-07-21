e59fc98a343d9c4a3fa16da1a68ac0d63edded90
Abbas Ali <absali6693@gmail.com>
2015-07-21 21:53:00 +0530

    Add user microposts

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'Google',
-      :aws_access_key_id     =>  '396044266782',
-      :aws_secret_access_key => '00b4903a9705f0cad42e0ead46b5b833efe353b4d38b0155fe44164bfaf3ab0c'
+      :google_access_key_id     =>  '396044266782',
+      :google_secret_access_key => '00b4903a9705f0cad42e0ead46b5b833efe353b4d38b0155fe44164bfaf3ab0c'
     }
     
   end
